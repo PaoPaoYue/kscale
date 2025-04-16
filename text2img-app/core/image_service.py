0a50b201115dde55d7fb5dde99196b1485bc9126
PaoPaoYue <846260131@qq.com>
2025-04-17 00:04:39 +1000

    test empty call

--- text2img-app/core/image_service.py
+++ text2img-app/core/image_service.py
@@ -28,18 +28,18 @@ class ImageService:
         width: int = Query(512),
         height: int = Query(512)
     ):
-        image, duration = self.generator.generate_image(prompt, steps, cfg_scale, sampler_index, width, height)
+        # image, duration = self.generator.generate_image(prompt, steps, cfg_scale, sampler_index, width, height)
 
-        print(f"Image {id} generation completed in {duration:.4f} seconds.")
+        # print(f"Image {id} generation completed in {duration:.4f} seconds.")
 
-        # 转 base64
-        buffer = io.BytesIO()
-        image.save(buffer, format="PNG")
-        encoded_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
+        # # 转 base64
+        # buffer = io.BytesIO()
+        # image.save(buffer, format="PNG")
+        # encoded_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
 
         return {
-            "image": encoded_image,
-            "duration": round(duration, 4)
+            "image": "encoded_image",
+            "duration": 0
         }
     
 entrypoint = ImageService.bind()

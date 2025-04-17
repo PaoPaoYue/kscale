bc7429a8dfb0fcf1ee7c5b3b4ceb72819c6ee1d7
PaoPaoYue <846260131@qq.com>
2025-04-17 17:33:32 +1000

    restructure

--- experiment/env.sh
+++ experiment/env.sh
@@ -2,5 +2,5 @@
 export API_ENDPOINT="http://35.206.113.234:30020"
 # export API_ENDPOINT="http://127.0.0.1:7860"
 export GENERATOR_ENDPOINT="http://35.206.113.234:30011"
-export TEST_NAME="test1"
+export TEST_NAME="mini1"
 export TEST_FILE="input/$TEST_NAME.csv"

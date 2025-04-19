0a5c1be8ae23c77268e0c1dc51a0d97eed307dfa
PaoPaoYue <846260131@qq.com>
2025-04-19 21:19:56 +1000

    add more result

--- experiment/smoke-test.sh
+++ experiment/smoke-test.sh
@@ -1,3 +1,3 @@
 . ./env.sh
 curl "$API_ENDPOINT/generate?index=0&prompt=A%20Mountain&steps=20&scale=7&sampler_index=DPM%2B%2B%202M%20Karras&width=512&height=512" \
-     | jq -r '.image' | base64 -d > output.png
+     | jq '.'
